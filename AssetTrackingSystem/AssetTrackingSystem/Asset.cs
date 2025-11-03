d537f62c3f9537c7aac87aa5e60a19759f16d4d9
connorstr <connor28072001@gmail.com>
2025-11-03 22:37:17 +0000

    Added an Asset entity class and a method for generating the unique ID

---
+++ AssetTrackingSystem/AssetTrackingSystem/Asset.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetTrackingSystem
+{
+    public class Asset
+    {
+        public int AssetID {  get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+        public string Type { get; set; }
+        public DateTime PurchaseDate { get; set; }
+        public string Note { get; set; }
+
+        public Asset() { }
+
+        public int generateID()
+        {
+            Random rnd = new Random();
+            return rnd.Next(1,9999);
+        }
+    }
+}
