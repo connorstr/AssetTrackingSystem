ef3d5995e751aa08ba9807df6a9f9ca6cbf3ca7f
connorstr <connor28072001@gmail.com>
2025-11-04 16:01:26 +0000

    Creating class for connection of the program to the database

---
+++ AssetTrackingSystem/AssetTrackingSystem/DatabaseManager.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetTrackingSystem
+{
+    public class DatabaseManager
+    {
+        private string connectionString;
+
+        public DatabaseManager()
+        {
+            connectionString = "server=lochnagar.abertay.ac.uk;user=sql2308259;password=already-patrol-finish-fight;database=sql2308259";
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(connectionString);
+        }
+    }
+}
