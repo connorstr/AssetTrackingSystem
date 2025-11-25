47f7ff942b7a05d92d2b3b4df4d7fd90e67f164c
connorstr <connor28072001@gmail.com>
2025-11-25 16:27:33 +0000

    added the system management packes to help grab the system details

--- AssetTrackingSystem/AssetTrackingSystem/SystemInfoHelper.cs
+++ AssetTrackingSystem/AssetTrackingSystem/SystemInfoHelper.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Management;
+using System.Net;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
