08d50c31715964b2643786bfeed723c1a5eda851
connorstr <connor28072001@gmail.com>
2025-11-28 15:02:09 +0000

    added methods for getting the system hardware details

--- AssetTrackingSystem/AssetTrackingSystem/SystemInfoHelper.cs
+++ AssetTrackingSystem/AssetTrackingSystem/SystemInfoHelper.cs
@@ -11,5 +11,46 @@ namespace AssetTrackingSystem
 {
     internal class SystemInfoHelper
     {
+        public static string GetSystemName()
+        {
+            return Environment.MachineName;
+        }
+
+        public static string GetManufacturer()
+        {
+            try
+            {
+                using var searcher = new ManagementObjectSearcher("SELECT Manufacturer FROM Win32_ComputerSystem");
+                var info = searcher.Get().Cast<ManagementObject>().FirstOrDefault();
+                return info?["Manufacturer"]?.ToString() ?? "Unknown";
+            }
+            catch { return "Unknown"; }
+        }
+
+        public static string GetModel()
+        {
+            try
+            {
+                using var searcher = new ManagementObjectSearcher("SELECT Model FROM Win32_ComputerSystem");
+                var info = searcher.Get().Cast<ManagementObject>().FirstOrDefault();
+                return info?["Model"]?.ToString() ?? "Unknown";
+            }
+            catch { return "Unknown"; }
+        }
+
+        public static string GetIPAddress()
+        {
+            try
+            {
+                var ipAddress = Dns.GetHostEntry(Dns.GetHostName())
+                .AddressList.FirstOrDefault(a => a.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork);
+
+                return ipAddress?.ToString() ?? "Unknown";
+            }
+            catch
+            {
+                return "Unknown";
+            }
+        }
     }
 }
