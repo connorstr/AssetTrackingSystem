ba8100ab5c2e59f44184e26527462f8788fe9856
connorstr <connor28072001@gmail.com>
2025-11-28 18:07:20 +0000

    missunderstood how the system.management package worked, readded the methods and fixed the errors

--- AssetTrackingSystem/AssetTrackingSystem/SystemInfoHelper.cs
+++ AssetTrackingSystem/AssetTrackingSystem/SystemInfoHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Management;
 using System.Net;
+using System.Net.Sockets;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,5 +24,57 @@ namespace AssetTrackingSystem
             };
         }
 
+        public static string GetSystemName()
+        {
+            return Environment.MachineName;
+        }
+
+        // Gets the hardware model
+        public static string GetModel()
+        {
+            try
+            {
+                using var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_ComputerSystem");
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    return obj["Model"]?.ToString() ?? "Unknown";
+                }
+            }
+            catch { }
+            return "Unknown";
+        }
+
+        // Gets the manufacturer (Dell, HP, Lenovo, etc.)
+        public static string GetManufacturer()
+        {
+            try
+            {
+                using var searcher = new ManagementObjectSearcher("SELECT * FROM Win32_ComputerSystem");
+                foreach (ManagementObject obj in searcher.Get())
+                {
+                    return obj["Manufacturer"]?.ToString() ?? "Unknown";
+                }
+            }
+            catch { }
+            return "Unknown";
+        }
+
+        // Gets the active IPv4 address of the machine
+        public static string GetIPAddress()
+        {
+            try
+            {
+                string ip = Dns.GetHostAddresses(Dns.GetHostName())
+                               .FirstOrDefault(a => a.AddressFamily == AddressFamily.InterNetwork)?
+                               .ToString();
+
+                return ip ?? "Unknown";
+            }
+            catch
+            {
+                return "Unknown";
+            }
+
+        }
     }
 }
