27c02b56bbb3615a56c468754f7adc9bd9fc468d
connorstr <connor28072001@gmail.com>
2025-12-01 16:54:23 +0000

    created a login form

---
+++ AssetTrackingSystem/AssetTrackingSystem/LoginForm.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace AssetTrackingSystem
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtEmail = new TextBox();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(313, 106);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(154, 23);
+            txtEmail.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(313, 153);
+            txtPassword.Name = "txtPassword";
+            txtPassword.PasswordChar = '*';
+            txtPassword.Size = new Size(154, 23);
+            txtPassword.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(313, 203);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(93, 28);
+            btnLogin.TabIndex = 2;
+            btnLogin.Text = "Log in";
+            btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtEmail);
+            Name = "LoginForm";
+            Text = "LoginForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtEmail;
+        private TextBox txtPassword;
+        private Button btnLogin;
+    }
+}
